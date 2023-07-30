496f3b59d00f062e857c059ece5246efb1774708
Schamper <1254028+Schamper@users.noreply.github.com>
2023-07-30 16:21:34 +0200

    Open EWF segments lazily when needed

--- tests/test_ewf.py
+++ tests/test_ewf.py
@@ -5,4 +5,4 @@ def test_ewf(ewf_data):
     e = ewf.EWF(ewf_data)
 
     assert e.size == 4097
-    assert e.read(4097) == (b"\xde\xad\xbe\xef" * 1024) + b"\n"
+    assert e.open().read(4097) == (b"\xde\xad\xbe\xef" * 1024) + b"\n"
